6f6c41ec5e2d9bac374d33e66cbf6f88ddb9d02a
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2021-05-24 20:05:41 +0430

    Project structure reorganized.

--- Back-End/Bank/BankService.cs
+++ Back-End/Bank/BankService.cs
@@ -4,9 +4,8 @@ namespace Back_End.Bank
 {
     public class BankService : IBankService
     {
-
-        readonly string ACCOUNTS_INDEX = "accounts";
-        readonly string TRANSACTIONS_INDEX = "transactions";
+        readonly string ACCOUNTS_INDEX = "accounts"; // todo : add this to appsettings.json
+        readonly string TRANSACTIONS_INDEX = "transactions"; // todo : add this to appsettings.json
         private IElastic elastic;
 
         public BankService(IElastic elastic)
--- Back-End/User/UsersService.cs
+++ Back-End/User/UsersService.cs
@@ -4,14 +4,13 @@ namespace Back_End.User
 {
     public class UsersService : IUsersService
     {
-        readonly string USERS_INDEX = "users";
+        readonly string USERS_INDEX = "users"; // todo : add this to appsettings.json
         private IElastic elastic;
 
         public UsersService(IElastic elastic)
         {
-            this.elastic = elastic;            
+            this.elastic = elastic;
             // check and create index if not created.
         }
-
     }
 }
