afcdde2dbc9910ecb2e378cda2334f5021290746
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2021-05-24 19:40:17 +0430

    model attributes added.

--- Back-End/Models/Account.cs
+++ Back-End/Models/Account.cs
@@ -2,6 +2,15 @@
 {
     public class Account
     {
-        
+        public string Id { get; set; }
+        public string CardId { get; set; }
+        public string Sheba { get; set; }
+        public string Type { get; set; }
+        public string BranchTelephone { get; set; }
+        public string BranchAddress { get; set; }
+        public string BranchName { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerFamily { get; set; }
+        public string OwnerId { get; set; }
     }
 }
--- Back-End/Models/Transaction.cs
+++ Back-End/Models/Transaction.cs
@@ -2,6 +2,13 @@
 {
     public class Transaction
     {
-        
+        public string SourceAccountId { get; set; }
+        public string DestinationAccountId { get; set; }
+        public string Date { get; set; } // todo : type correction
+        public string Time { get; set; } // todo : type correction
+        public string TrackingId { get; set; }
+        public long Amount { get; set; }
+        public string Type { get; set; }
+        public string TransactionId { get; set; }
     }
 }
