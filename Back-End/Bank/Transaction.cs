f54f6c04042bf66864511a90274dc5111f0963e2
amirhessam <amirhessamb123@yahoo.com>
2021-05-27 10:59:07 +0430

    Transaction's date edited

--- Back-End/Bank/Transaction.cs
+++ Back-End/Bank/Transaction.cs
@@ -1,11 +1,25 @@
-﻿namespace Back_End.Bank
+﻿using System;
+using System.Data;
+
+namespace Back_End.Bank
 {
     public class Transaction
     {
         public string SourceAccountId { get; set; }
         public string DestinationAccountId { get; set; }
-        public string Date { get; set; } // todo : type correction
-        public string Time { get; set; } // todo : type correction
+        public DateTime Date { get; set; } // todo : type correction
+        
+        public void SetDate(string timeString)
+        {
+            Time = DateTime.ParseExact(timeString, "yyyy-MM-dd HH:mm",null);
+        }
+        public DateTime Time { get; set; } // todo : type correction
+
+       
+        public void SetTime(string timeString)
+        {
+            Time = DateTime.ParseExact(timeString, "yyyy-MM-dd HH:mm:ss",null);
+        }
         public string TrackingId { get; set; }
         public long Amount { get; set; }
         public string Type { get; set; }
