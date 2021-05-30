713efc404b997c34cfacb33f0cceec4b6d7ead27
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2021-05-30 21:45:55 +0430

    NGX Cookie Service added.

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
-import { SingleInputFormComponent } from './login/single-input-form/single-input-form.component';
-import { MatIconModule } from '@angular/material/icon';
-import { ColorTestComponent } from './color-test/color-test.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {LoginComponent} from './login/login.component';
+import {SingleInputFormComponent} from './login/single-input-form/single-input-form.component';
+import {MatIconModule} from '@angular/material/icon';
+import {ColorTestComponent} from './color-test/color-test.component';
 import {HttpClientModule} from '@angular/common/http';
+import {CookieService} from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, SingleInputFormComponent, ColorTestComponent],
@@ -28,7 +29,8 @@ import {HttpClientModule} from '@angular/common/http';
     FormsModule,
     MatIconModule,
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
