d6ef0c822877019759db39163738823157749be7
Reza Adibi <adbrezaooo@gmail.com>
2021-06-03 18:08:56 +0430

    Some modules removed.

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListViewPageComponent } from './list-view-page/list-view-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   {
     path: 'login',
+    pathMatch: 'full',
     component: LoginPageComponent,
   },
   {
     path: 'listview',
     pathMatch: 'full',
-    loadChildren: () =>
-      import('./list-view-page/list-view-page.module').then(
-        (m) => m.ListViewPageModule
-      ),
+    component: ListViewPageComponent,
   },
 ];
 
--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -10,19 +10,29 @@ import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CookieService } from 'ngx-cookie-service';
-import { ListViewPageModule } from './list-view-page/list-view-page.module';
-import { SingleInputFormComponent } from './login-page/single-input-form/single-input-form.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SingleInputFormComponent } from './login-page/single-input-form/single-input-form.component';
+import { ListViewPageComponent } from './list-view-page/list-view-page.component';
+import { TransactionsListComponent } from './list-view-page/transactions-list/transactions-list.component';
+import { FilterMenuComponent } from './list-view-page/filter-menu/filter-menu.component';
+import { SiteNavigationMenuComponent } from './site-navigation-menu/site-navigation-menu.component';
 
 @NgModule({
-  declarations: [AppComponent, SingleInputFormComponent, LoginPageComponent],
+  declarations: [
+    AppComponent,
+    SingleInputFormComponent,
+    LoginPageComponent,
+    TransactionsListComponent,
+    ListViewPageComponent,
+    FilterMenuComponent,
+    SiteNavigationMenuComponent,
+  ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,
-    ListViewPageModule,
     FormsModule,
+    HttpClientModule,
     MatButtonModule,
     MatFormFieldModule,
     MatIconModule,
